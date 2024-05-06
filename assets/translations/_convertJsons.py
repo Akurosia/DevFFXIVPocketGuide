b8e1c2c1565ce89c2d7b8c35d6e5d321c5925423
Akurosia Kamo <kamothegamer@web.de>
2024-05-06 10:34:28 +0200

    new header translations

--- assets/translations/_convertJsons.py
+++ assets/translations/_convertJsons.py
@@ -5,7 +5,7 @@ for lang in ['en-US', 'de-DE', 'fr-FR', 'ja-JP', 'cn-CN', 'ko-KR']:
     new_data = {}
     files = glob(f"*/{lang}.json", recursive=True)
     for _file in files:
-        print(_file)
+        #print(_file)
         new_data = dict(new_data, **readJsonFile(_file))
 
     writeJsonFile(f"{lang}.json", new_data)
