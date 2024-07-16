98e25cb118a469ee7f8a167718911b0bb4a07650
Akurosia Kamo <kamothegamer@web.de>
2024-07-16 14:03:41 +0200

    fix fflogs pls?

--- assets/js/fflogs.js
+++ assets/js/fflogs.js
@@ -253,14 +253,14 @@ jobs = {
     "Ninja":       "062130_hr1.png",
     "Samurai":     "062134_hr1.png",
     "Reaper":      "062139_hr1.png",
-    "Viper":       "",
+    "Viper":       "062141_hr1.png",
     "Bard":        "062123_hr1.png",
     "Machinist":   "062131_hr1.png",
     "Dancer":      "062138_hr1.png",
     "BlackMage":   "062125_hr1.png",
     "Summoner":    "062127_hr1.png",
     "RedMage":     "062135_hr1.png",
-    "Pictomancer": ""
+    "Pictomancer": "062142_hr1.png"
 }
 //golden icons
 jobs = {
@@ -277,14 +277,14 @@ jobs = {
     "Ninja":       "062030_hr1.png",
     "Samurai":     "062034_hr1.png",
     "Reaper":      "062039_hr1.png",
-    "Viper":       "062141_hr1.png",
+    "Viper":       "062041_hr1.png",
     "Bard":        "062023_hr1.png",
     "Machinist":   "062031_hr1.png",
     "Dancer":      "062038_hr1.png",
     "BlackMage":   "062025_hr1.png",
     "Summoner":    "062027_hr1.png",
     "RedMage":     "062035_hr1.png",
-    "Pictomancer": "062142_hr1.png"
+    "Pictomancer": "062042_hr1.png"
 }
 function addJobImage(job){
     return '<img src="https://xivapi.com/i/062000/' + jobs[job] + '" style="height: 30px; padding-right: 5px;">'
