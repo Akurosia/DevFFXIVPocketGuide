f34d93c5b9559230af953647757561c9b24bf231
Akurosia Kamo <kamothegamer@web.de>
2024-07-23 23:27:24 +0200

    some more dalamud stuff

--- python_scripts/get_dalamud_plugins.py
+++ python_scripts/get_dalamud_plugins.py
@@ -60,26 +60,41 @@ def getFinalData():
                 if value.get("punchline", None):
                     pun = value["punchline"][0].replace('"', "'")
                     write(out, f'   punchline: "{pun}"')
+                if value.get("DalamudApiLevel", None):
+                    write(out, f'   DalamudApiLevel: {value["DalamudApiLevel"]}')
                 if value.get("tags", None):
                     write(out, f'   tags:')
-                    for tag in value["tags"][0]:
+                    for tag in value["tags"]:
                         write(out, f'     - tag: "{tag}"')
                 write(out, f'   versions:')
+                #create dict as version > (author, url, repo)
                 tmp = {}
                 for v in value["versions"]:
-                    author, url, version = v
+                    author, url, repo, version = v
                     if not tmp.get(version, None):
                         tmp[version] = []
-                    _tuple = (author, url)
+                    _tuple = (author, url, repo)
                     tmp[version].append(_tuple)
                 #versions = [k for k in tmp].sort(key=StrictVersion)
                 versions = natsorted([k for k in tmp],reverse=True)
                 for version in versions:
-                    for x in tmp[version]:
-                        author, url = x
+                    #create dict as url > (author, repo)
+                    tmp2 = {}
+                    for u in tmp[version]:
+                        author, url, repo = u
+                        if not tmp2.get(url, None):
+                            tmp2[url] = []
+                        _tuple = (author, repo)
+                        tmp2[url].append(_tuple)
+                    urls = natsorted([k for k in tmp2])
+                    #print_color_green(urls)
+                    for url1 in urls:
+                        #print_color_green(tmp2[url1][0])
+                        author, repo = tmp2[url1][0]
                         write(out, f'     - author: "{author}"')
-                        write(out, f'       url: "{url}"')
+                        write(out, f'       url: "{url1}"')
                         write(out, f'       version: "{version}"')
+                        write(out, f'       repo: "{repo}"')
             except Exception as e:
                 print_color_red(e)
 
@@ -91,7 +106,7 @@ def run():
     completed_repos = []
     repos_with_load_errors = []
     content = readJsonFile(r"C:\Users\kamot\AppData\Roaming\XIVLauncher\dalamudConfig.json")
-    result = readJsonFile("dalamud_repos.json")
+    result = {}#readJsonFile("dalamud_repos.json")
     # collect all urls from local dalamud file
     repo_urls = []
     for key in content["ThirdRepoList"]['$values']:
@@ -125,16 +140,22 @@ def run():
                     result[entry['Name']] = {
                         "name": entry['Name'],
                         "description": entry.get('Description', ""),
-                        "versions": []
+                        "versions": [],
+                        "tags": [],
+                        "DalamudApiLevel": entry.get('DalamudApiLevel', 0)
                     }
                     if entry.get('Punchline', None):
                         result[entry['Name']]["punchline"] = entry['Punchline'],
                     if entry.get('Tags', None):
-                        result[entry['Name']]["tags"] = entry['Tags'],
+                        result[entry['Name']]["tags"] += entry['Tags'][0],
+                    if entry.get('CategoryTags', None):
+                        result[entry['Name']]["tags"] += entry['CategoryTags'][0],
                     if entry.get('ImageUrls', None):
                         result[entry['Name']]["image"] = entry['ImageUrls'][0],
 
-                _tuple = (entry['Author'], url, entry.get('AssemblyVersion', 0))
+                _tuple = (entry['Author'], url, entry.get('RepoUrl', None), entry.get('AssemblyVersion', 0))
+                if int(result[entry['Name']].get("DalamudApiLevel", 0)) < int(entry.get('DalamudApiLevel', 0)):
+                    result[entry['Name']]["DalamudApiLevel"] = int(entry["DalamudApiLevel"])
                 result[entry['Name']]["versions"].append(_tuple)
         else:
             print(f"Error on: {url}")
