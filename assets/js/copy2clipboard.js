f715ad92d4710dbd2053b843226ba7a0a27eac5f
Akurosia <Akurosia@users.noreply.github.com>
2024-10-26 20:40:03 +0000

    asd 692f5fb7687f52d2545c2e14d7c13836b01fee41

---
+++ assets/js/copy2clipboard.js
@@ -0,0 +1,29 @@
+function copyValuetoClipboard(value){
+    var textarea = document.createElement("textarea");
+    textarea.textContent = value;
+    textarea.style.position = "fixed";
+    document.body.appendChild(textarea);
+    textarea.select();
+    try {
+       return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+    } catch (ex) {
+       console.warn("Copy to clipboard failed.", ex);
+       return false;
+    } finally {
+       console.log("copied value: " + value)
+       document.body.removeChild(textarea);
+    }
+}
+
+function buildAndCopyRegex(_id, bossname, type){
+  if (type == "attack"){
+    copyValuetoClipboard(" (14|15|16):[^:]*:[^:]*:" + _id + ":[^:]*:[^:]*:"+bossname)
+  }else if(type == "debuff"){
+    copyValuetoClipboard(":" + _id + ":" + bossname)
+  }
+}
+
+
+function copyIcon(path){
+  copyValuetoClipboard(path)
+}
