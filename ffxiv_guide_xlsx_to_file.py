b3c39cef62cec04656c8267d75c567beade4315e
Akurosia Kamo <kamothegamer@web.de>
2025-08-16 12:13:26 +0200

    add forked tower

--- ffxiv_guide_xlsx_to_file.py
+++ ffxiv_guide_xlsx_to_file.py
@@ -261,10 +261,10 @@ def run(googledata: dict[str, EntryType], orderedContent: dict[str, str]) -> Non
         i = int(key)
         try:
             debug_row_number = i
-            if not True:
+            if True:
                 #if debug_row_number < 700 :
                 #if debug_row_number not in [354, 581, 787, 788, 818, 821]:
-                if debug_row_number not in [456]:
+                if debug_row_number not in [852]:
                     print_debug = True
                     continue
             entry: EntryType = getEntryData(value, i, orderedContent)
