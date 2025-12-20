aa3789a5f34be88b6ec36ccf63ecfc25a9565903
Akurosia Kamo <kamothegamer@web.de>
2025-12-20 12:46:29 +0100

    boss

--- ffxiv_guide_xlsx_to_file.py
+++ ffxiv_guide_xlsx_to_file.py
@@ -263,7 +263,7 @@ def run(googledata: dict[str, EntryType], orderedContent: dict[str, str]) -> Non
         i = int(key)
         try:
             debug_row_number = i
-            if True:
+            if not True:
                 #if debug_row_number < 800 :
                 #if debug_row_number not in [354, 581, 787, 788, 818, 821]:
                 if debug_row_number not in [815, 835,836,837,838,839]:
